ada98d5d235925996183b89168d1688ab521fede
Michael Muehlberghuber <mm@muem.at>
2016-03-08 22:00:22 +0100

    Add a small header to the batch file for watermarking images on the bottom right corner.

--- watermark-bottom_right.bat
+++ watermark-bottom_right.bat
@@ -1,8 +1,23 @@
+:: ABOUT:
+:: This batch script is intended to create a watermark on the bottom right
+:: corner of an image. It can be useful to create a copyright watermark in an
+:: image before publishing it on, for instance, the Internet. Independently of
+:: the size of the image you want to process, the resulting watermark will
+:: always be inserted using a size proportionally to the input image size (i.e.,
+:: the ratio between the size of the watermark and the image will remain
+:: constant). 
+
+:: REQUIREMENTS:
+:: - You need to have ImageMagick installed on your windows system.
+:: - A vector image representing your watermark.
+
+:: USAGE:
+:: - Take a look at the variables below, which can be used to adjust the size as
+::   well as the position of the watermark.
+:: - Simply drop an image (e.g., a *.jpg) on the batch file.
+
 :: Disable echoing commands.
 @echo off
-
-:: Apply watmark to all images dropped on batch file.
-set SRC=%1
 
 :: Width of the watermark in percent of the original image width.
 set WMWIDTH=15
@@ -15,6 +30,9 @@ set WMOFFSET=3
 set POSTFIX=_wm
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: Apply watmark to the image being dropped on the batch file.
+set SRC=%1
 
 :: Get the base name of the image.
 FOR /F %%i IN ("%SRC%") DO set SRCBASE=%%~ni
